1f1fa6aea15d77e5d82e7e88c642e0a0f912d4f4
lvming <lvming@zztabc.com>
2023-12-28 15:58:14 +0800

    feat: 添加 CellTitle 组件

---
+++ src/components/CellTitle/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { CellTitleProps } from './types'
+import { PREFIX } from './constant'
+
+withDefaults(defineProps<CellTitleProps>(), {
+  padding: 30,
+})
+const slots = useSlots()
+</script>
+
+<template>
+  <view class="cell-title" :style="{ padding: `${padding}rpx` }">
+    <view class="flex-center pb-30rpx">
+      <view v-if="slots.icon" class="mr-16rpx">
+        <slot name="icon" />
+      </view>
+      <view class="w-[95%] ellipsis">
+        <slot>
+          <text class="ellipsis" :class="[`${PREFIX}__label`]">
+            {{ label }}
+          </text>
+        </slot>
+        <br>
+        <slot name="sub-label">
+          <text :class="[`${PREFIX}__sub-label`]">
+            {{ subLabel }}
+          </text>
+        </slot>
+      </view>
+    </view>
+    <uv-line v-if="border" />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.cell-title {
+  --uno: w-full box-border;
+
+  &__label {
+    --uno: text-34rpx text-[#202020] font-500;
+  }
+
+  &__sub-label {
+    --uno: text-[#9F9F9F] text-24rpx mr-16rpx;
+  }
+}
+</style>
---
+++ src/components/CellTitle/types.ts
@@ -0,0 +1,7 @@
+export interface CellTitleProps {
+  label?: string
+  subLabel?: string
+  border?: boolean
+  padding?: number
+  icon?: string
+}
