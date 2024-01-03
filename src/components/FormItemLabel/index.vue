ca1160eabc283daadceec99b55b4e3758a38877f
lvming <lvming@zztabc.com>
2024-01-03 17:28:15 +0800

    feat: 封装form item label 插槽

---
+++ src/components/FormItemLabel/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Props } from './types.ts'
+
+defineProps<Props>()
+</script>
+
+<template>
+  <view class="uv-form-item__body__left">
+    <view class="uv-form-item__body__left__content" :class="ownClass">
+      <view class="uv-form-item__body__left__content__label">
+        <slot />
+      </view>
+    </view>
+  </view>
+</template>
---
+++ src/components/FormItemLabel/types.ts
@@ -0,0 +1,3 @@
+export interface Props {
+  ownClass?: string
+}
